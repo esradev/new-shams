404888a44a4d329aaeab60db6deacdfe7f24f6d5
wpstorm <wpstormdev@gmail.com>
2025-12-03 21:08:11 +0330

    Remove CategoryList component and usage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,6 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 import { Href } from "expo-router"
 import { useColorScheme } from "nativewind"
 
-import CategoryList from "@/components/category-list"
 import CourseCard from "@/components/course-card"
 import { useApi } from "@/context/api-context"
 import GlobalError from "@/components/global-error"
@@ -23,27 +22,6 @@ export default function HomePage() {
         ) : (
           <ScrollView showsVerticalScrollIndicator={false}>
             <View className="p-6">
-              <View className="mb-8">
-                <Text className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200 text-right dir-rtl">
-                  موضوعات دروس
-                </Text>
-                <ScrollView
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  className="flex flex-row mr-2 flex-1 pb-2 scrollbar-hide text-right dir-rtl"
-                >
-                  {categories
-                    .filter(category => category.parent === 0)
-                    .map(category => (
-                      <CategoryList
-                        key={category.id}
-                        href={`/categories/${category.id}` as Href}
-                        category={category}
-                      />
-                    ))}
-                </ScrollView>
-              </View>
-
               <View className="mb-8">
                 <Text className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200 text-right dir-rtl">
                   تمام دروس خارج
